ffca9d2ae0fa953828b635dd705c5b9a310f75db
Jelle de Vries <jelle_de_vriesie@hotmail.com>
2018-04-03 13:41:54 +0200

    Reader parser toegevoegd

--- src/com/company/Reader.java
+++ src/com/company/Reader.java
@@ -1,17 +1,35 @@
 package com.company;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.util.AbstractMap;
 import java.util.ArrayList;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Reader implements Runnable {
     Socket socket;
     BufferedReader inputStream;
-    ArrayList<String> queue;
+    ArrayList<Map.Entry<String, ArrayList<String>>> queue;
+    private static final String[][] responses = {
+            {"SVR GAMELIST", "GAMES"},
+            {"SVR PLAYERLIST", "PLAYERS"},
+            {"SVR GAME MATCH", "MATCH"},
+            {"SVR GAME YOURTURN", "YOURTURN"},
+            {"SVR GAME MOVE", "MOVE"},
+            {"SVR GAME WIN", "WIN"},
+            {"SVR GAME LOSS", "LOSS"},
+            {"SVR GAME CHALLENGE", "CHALLENGE"},
+            {"SVR GAME CHALLENGE CANCELLED", "CANCELLED"}
+    };
 
     public Reader(Socket socket) {
         this.socket = socket;
@@ -27,44 +45,50 @@ public class Reader implements Runnable {
     @Override
     public void run() {
         String line;
-        Pattern positionPattern = Pattern.compile("MOVE: \"([^}]*)\", DETAILS:");
-        Pattern playerPattern = Pattern.compile("PLAYER: \"([^}]*)\", MOVE:");
-
-        Matcher matcher;
-        Matcher matcher2;
 
         while(true) {
             try {
                 if ((line = inputStream.readLine()) != null) {
-                    System.out.println(line);
-                    queue.add(line);
-                    if(line.startsWith("SVR GAME MOVE")) {
-                        matcher = positionPattern.matcher(line);
-                        if(matcher.find()) {
-                            int turn = Integer.parseInt(matcher.group(1));
-                            System.out.println("Turn: " + turn);
-
-                            matcher2 = playerPattern.matcher(line);
-                            int selfOrOpp = 1;
-                            if(matcher2.find()) {
-                                String name = matcher2.group(1);
-                                if(name.equals("tester")) {
-                                    selfOrOpp = 1;
-                                }
-                                else {
-                                    selfOrOpp = 2;
-                                }
-                            }
-                        }
-                    }
-                    if(line.startsWith("SVR GAME YOURTURN")) {
-                        System.out.println("Your turn:");
-                    }
+                    queue.add(parseResponse(line));
                 }
             }
             catch(IOException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    private Map.Entry<String, ArrayList<String>> parseResponse(String response) {
+        // If "OK" -> return "OK"
+        if(response.equals("OK")) {
+            return new AbstractMap.SimpleEntry<>("OK", new ArrayList<>());
+        }
+
+        String key;
+        ArrayList<String> values = new ArrayList<>();
+
+        JsonParser parser = new JsonParser();
+        JsonElement obj;
+
+        for(String[] prefix: responses) {
+            if(response.startsWith(prefix[0])) {
+                key = prefix[1];
+                response = response.replace(prefix[0]+" ", "");
+                obj = parser.parse(response);
+
+                if(obj.isJsonArray()) {
+                    values = new Gson().fromJson(obj, ArrayList.class);
+                }
+                else if(obj.isJsonObject()) {
+                    JsonObject jsonObject = (JsonObject)obj;
+                    for(Map.Entry<String, JsonElement> entry : jsonObject.entrySet()) {
+                        values.add(entry.getValue().toString());
+                    }
+                }
+
+                return new AbstractMap.SimpleEntry<>(key, values);
+            }
+        }
+        return null;
     }
 }
